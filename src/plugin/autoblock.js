2a25061c5c0d94c395091f4e59916c8a4f4b8dce
Fareno237 <lerfareno@gmail.com>
2024-09-27 16:50:57 +0100

    Update autoblock.js

--- src/plugin/autoblock.js
+++ src/plugin/autoblock.js
@@ -8,7 +8,7 @@ const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0
 const text = m.body.slice(prefix.length + cmd.length).trim();
 
   if (cmd === 'autoblock') {
-    if (!isCreator) return m.reply("*📛 THIS IS AN OWNER COMMAND*");
+    if (!isCreator) return m.reply("*🚫 THIS IS AN OWNER COMMAND*");
     let responseMessage;
 
     if (text === 'on') {
