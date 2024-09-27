44cd4ee1f6970c0c35f6f469cf100c8c971e92c2
Fareno237 <lerfareno@gmail.com>
2024-09-27 16:45:56 +0100

    Update demote.js

--- src/plugin/demote.js
+++ src/plugin/demote.js
@@ -11,15 +11,15 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
     if (!validCommands.includes(cmd)) return;
 
 
-    if (!m.isGroup) return m.reply("*📛 THIS COMMAND CAN ONLY BE USED IN GROUPS*");
+    if (!m.isGroup) return m.reply("*🚫 THIS COMMAND CAN ONLY BE USED IN GROUPS*");
     const groupMetadata = await gss.groupMetadata(m.from);
     const participants = groupMetadata.participants;
     const botNumber = await gss.decodeJid(gss.user.id);
     const botAdmin = participants.find(p => p.id === botNumber)?.admin;
     const senderAdmin = participants.find(p => p.id === m.sender)?.admin;
 
-    if (!botAdmin) return m.reply("*📛 BOT MUST BE AN ADMIN TO USE THIS COMMAND*");
-    if (!senderAdmin) return m.reply("*📛 YOU MUST BE AN ADMIN TO USE THIS COMMAND*");
+    if (!botAdmin) return m.reply("*🚫 BOT MUST BE AN ADMIN TO USE THIS COMMAND*");
+    if (!senderAdmin) return m.reply("*🚫 YOU MUST BE AN ADMIN TO USE THIS COMMAND*");
 
     if (!m.mentionedJid) m.mentionedJid = [];
 
@@ -32,7 +32,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
       : [];
 
     if (users.length === 0) {
-      return m.reply("*📛 PLEASE MENTION OR QUOTE A USER TO DEMOTE*");
+      return m.reply("*🚫 PLEASE MENTION OR QUOTE A USER TO DEMOTE*");
     }
 
     const validUsers = users.filter(Boolean);
