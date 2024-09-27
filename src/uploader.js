d7118cd65d61e43913d3fd9adf3a34f0fc5907a4
Fareno237 <lerfareno@gmail.com>
2024-09-27 16:56:09 +0100

    Update uploader.js

--- src/uploader.js
+++ src/uploader.js
@@ -78,7 +78,7 @@ export const webp2mp4File = async (path) => {
       const result = 'https:' + $2('div#output > p.outfile > video > source').attr('src');
       resolve({
         status: true,
-        message: "Created By Ethix-MD",
+        message: "Created By ☞⌜ KG TECH⌝☜",
         result: result
       });
     } catch (err) {
