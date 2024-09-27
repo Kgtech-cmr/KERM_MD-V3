8ce99755f4c7e9b434554c2da7661af2fa97259c
Fareno237 <lerfareno@gmail.com>
2024-09-27 16:37:00 +0100

    Update gcfullpp.js

--- src/plugin/gcfullpp.js
+++ src/plugin/gcfullpp.js
@@ -11,15 +11,15 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
 
   if (validCommands.includes(cmd)) {
     
-    if (!m.isGroup) return m.reply("*📛 THIS COMMAND CAN ONLY BE USED IN GROUPS*");
+    if (!m.isGroup) return m.reply("*🚫 THIS COMMAND CAN ONLY BE USED IN GROUPS*");
     const groupMetadata = await gss.groupMetadata(m.from);
     const participants = groupMetadata.participants;
     const botNumber = await gss.decodeJid(gss.user.id);
     const botAdmin = participants.find(p => p.id === botNumber)?.admin;
     const senderAdmin = participants.find(p => p.id === m.sender)?.admin;
 
-    if (!botAdmin) return m.reply("*📛 BOT MUST BE AN ADMIN TO USE THIS COMMAND*");
-    if (!senderAdmin) return m.reply("*📛 YOU MUST BE AN ADMIN TO USE THIS COMMAND*");
+    if (!botAdmin) return m.reply("*🚫 BOT MUST BE AN ADMIN TO USE THIS COMMAND*");
+    if (!senderAdmin) return m.reply("*🚫 YOU MUST BE AN ADMIN TO USE THIS COMMAND*");
     if (!m.quoted || m.quoted.mtype !== 'imageMessage') {
       return m.reply(`Send/Reply with an image to set your profile picture ${prefix + cmd}`);
     }
