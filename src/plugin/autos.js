8d5cc84b222d2c887faea96c704543546f8755eb
Fareno237 <lerfareno@gmail.com>
2024-09-27 16:48:39 +0100

    Update autos.js

--- src/plugin/autos.js
+++ src/plugin/autos.js
@@ -11,7 +11,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
   const validCommands = ['autostatus', 'autosview', 'autostatusview'];
 
  if (validCommands.includes(cmd)){
-   if (!isCreator) return m.reply("*📛 THIS IS AN OWNER COMMAND*");
+   if (!isCreator) return m.reply("*⚠️ THIS IS AN OWNER COMMAND*");
     let responseMessage;
 
     if (text === 'on') {
