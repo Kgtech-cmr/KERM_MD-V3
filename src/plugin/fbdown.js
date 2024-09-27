7e2c66f51b4c3ddb3bf27f31c0f78337cb791c89
Fareno237 <lerfareno@gmail.com>
2024-09-27 16:03:12 +0100

    Update fbdown.js

--- src/plugin/fbdown.js
+++ src/plugin/fbdown.js
@@ -146,7 +146,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
             content = { 
               video: finalMediaBuffer, 
               mimetype: 'video/mp4', 
-              caption: '> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴇᴛʜɪx-ᴍᴅ',
+              caption: '`© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋᴇʀᴍ-ᴍᴅ`',
             };
             await Matrix.sendMessage(m.from, content, { quoted: m });
           } else {
