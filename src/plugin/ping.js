40da43ee802045b586c31ef36aea4181a1a03ef0
Fareno237 <lerfareno@gmail.com>
2024-09-27 16:22:42 +0100

    Update ping.js

--- src/plugin/ping.js
+++ src/plugin/ping.js
@@ -7,11 +7,11 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
 
   if (cmd === "ping") {
     const start = new Date().getTime();
-    await m.React('⚡');
+    await m.React('😁');
     const end = new Date().getTime();
     const responseTime = (end - start) / 1000;
 
-    const text = `*_🔥⃝вσт ѕρєє∂: ${responseTime.toFixed(2)} s_*`;
+    const text = `*𝙆𝙀𝙍𝙈_𝙈𝘿-𝙑3 running: ${responseTime.toFixed(2)} s_*`;
     sock.sendMessage(m.from, { text }, { quoted: m });
   }
 }
