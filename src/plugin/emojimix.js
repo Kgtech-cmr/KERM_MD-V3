f52c8ada4bedd114b610463f6ca501b111246d37
Fareno237 <lerfareno@gmail.com>
2024-09-27 16:45:14 +0100

    Update emojimix.js

--- src/plugin/emojimix.js
+++ src/plugin/emojimix.js
@@ -25,7 +25,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
     }
 
     for (let res of anu.results) {
-      const encmedia = await Matrix.sendImageAsSticker(m.from, res.url, m, { packname: "", author: "Ethix-MD", categories: res.tags });
+      const encmedia = await Matrix.sendImageAsSticker(m.from, res.url, m, { packname: "", author: "☞⌜ KG TECH⌝☜", categories: res.tags });
       await fs.unlinkSync(encmedia);
     }
   } catch (error) {
