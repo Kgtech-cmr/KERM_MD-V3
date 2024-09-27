4a0b2d55924ff4c37fe8331cd1cddc395c750807
Fareno237 <lerfareno@gmail.com>
2024-09-27 16:41:33 +0100

    Update fetch.js

--- src/plugin/fetch.js
+++ src/plugin/fetch.js
@@ -23,7 +23,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
 
       const contentType = res.headers.get('content-type');
       if (!/text|json/.test(contentType)) {
-        await Matrix.sendMedia(m.from, url, 'file', '> Api Fetched From Ethix-MD', m);
+        await Matrix.sendMedia(m.from, url, 'file', '> Api Fetched From ✭𝙺𝙴𝚁𝙼 𝙼𝙳 𝚅3✭', m);
         return;
       }
 
