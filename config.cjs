791ef5d31a2dbde9143643ab6778066c9f7e948e
STRIKERBOY <110353280+Devenlee350@users.noreply.github.com>
2024-09-28 21:05:31 +0200

    𝚂𝚃𝚁𝙸𝙺𝙴𝚁𝙱𝙾𝚈

--- config.cjs
+++ config.cjs
@@ -20,7 +20,7 @@ const config = {
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "☞⌜𝚂𝚃𝚁𝙸𝙺𝙴𝚁𝙱𝙾𝚈⌝☜",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "237659535227",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "263784562833",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
