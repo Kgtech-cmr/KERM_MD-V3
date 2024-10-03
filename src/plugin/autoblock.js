b202a20a6b18c0c408aa897dc97abebd544ddfcf
Fareno237 <lerfareno@gmail.com>
2024-10-03 02:33:18 +0100

    Update autoblock.js

--- src/plugin/autoblock.js
+++ src/plugin/autoblock.js
@@ -1,4 +1,4 @@
-import config from '../../config.cjs';
+import config from '../../config.js';
 
 const autoblockCommand = async (m, Matrix) => {
   const botNumber = await Matrix.decodeJid(Matrix.user.id);
