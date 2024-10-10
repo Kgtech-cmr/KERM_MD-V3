2b4fbccf996d1cfed666a86e28ab2f09d3157aa9
Janurapahan37 <janurapahan37@gmail.com>
2024-10-10 06:27:58 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -18,9 +18,9 @@ const config = {
   
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
-  MODE: process.env.MODE || "public",
+  MODE: process.env.MODE || "private",
   OWNER_NAME: process.env.OWNER_NAME || "☞⌜ KG TECH⌝☜",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "237659535227",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "94768809537",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
